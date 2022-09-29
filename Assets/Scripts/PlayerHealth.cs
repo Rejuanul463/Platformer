b4bb716deb23b6b1f85b010ac7eb6dccd37bb88a
dar1us_7 <rejuanulhuq@gmail.com>
2022-09-29 16:24:54 +0600

    restartMethod

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public GameObject canvas;
+    private Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("obstacle"))
         {
-            Destroy(gameObject);
+            canvas.SetActive(true);
+            transform.position = pos;
+            gameObject.SetActive(false);
         }
     }
 }
